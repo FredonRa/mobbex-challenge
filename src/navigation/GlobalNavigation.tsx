1d1f6b92b4031fe5e816daf302fb0bced8c75f50
Federico Leiva <fedeleiva20@gmail.com>
2022-07-14 18:43:11 -0300

    Agrego navegación básica

---
+++ src/navigation/GlobalNavigation.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { createStackNavigator } from '@react-navigation/stack'
+import Products from '../screens/Products';
+import ProductDetail from '../screens/ProductDetail';
+import { HOME_PRODUCTS, HOME_PRODUCT_DETAIL } from '../constants/routes';
+
+const Stack = createStackNavigator();
+
+const GlobalNavigation: React.FC = () => {
+    return (  
+        <Stack.Navigator>
+            <Stack.Screen name={HOME_PRODUCTS} component={Products} />  
+            <Stack.Screen name={HOME_PRODUCT_DETAIL} component={ProductDetail} />
+        </Stack.Navigator>
+    );
+}
+ 
+export default GlobalNavigation;
